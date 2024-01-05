6f5a5bce8afeb51c6808ec003fa5fc8b60878bce
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-01-05 20:01:19 +0200

    Add project files.

---
+++ WpfApp3/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class UserColor
+    {
+        public int Red { set; get; }
+        public int Green { set; get; }
+        public int Blue { set; get; }
+        public int Alpha {  set; get; }
+    }
+}
---
+++ WpfApp3/MainWindowViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Input;
+
+namespace WpfApp3
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        int _red, _green, _blue, _alpha;
+        public int CurrentRed
+        {
+            get
+            {
+                return _red;
+            }
+            set
+            {
+                _red = value;
+                RaisePropertyChanged(nameof(CurrentRed));
+            }
+        }
+        public int CurrentGreen
+        {
+            get
+            {
+                return _green;
+            }
+            set
+            {
+                _green = value;
+                RaisePropertyChanged(nameof(CurrentGreen));
+            }
+        }
+        public int CurrentBlue
+        {
+            get 
+            {
+                return _blue;
+            }
+            set
+            {
+                _blue = value;
+                RaisePropertyChanged(nameof(CurrentBlue));
+            }
+        }
+        public int CurrentAlpha
+        {
+            get
+            {
+                return _alpha;
+            }
+            set
+            {
+                _alpha = value;
+                RaisePropertyChanged(nameof(CurrentAlpha));
+            }
+        }
+
+        public MainWindowViewModel() {
+            CurrentRed = 0;
+            CurrentGreen = 0;
+            CurrentBlue = 0;
+            CurrentAlpha = 0;
+        }
+
+        private ObservableCollection<Color> _colors = new ObservableCollection<Color>();
+        public ObservableCollection<Color> Colors
+        {
+            get
+            {
+                return _colors;
+            }
+            set
+            {
+                _colors = value;
+                RaisePropertyChanged(nameof(Colors));
+            }
+        }
+
+        private DelegateCommand _AddColorCommand;
+        public ICommand AddColor
+        {
+            get
+            {
+                if(_AddColorCommand == null)
+                {
+                    _AddColorCommand = new DelegateCommand(Add, CanAdd);
+                }
+                return _AddColorCommand;
+            }
+        }
+        private void Add(object parameter)
+        {
+            _colors.Add(Color.FromArgb((byte)CurrentAlpha, (byte)CurrentRed, (byte)CurrentGreen, (byte)CurrentBlue));
+            CurrentAlpha = 0;
+            CurrentBlue = 0;
+            CurrentGreen = 0;
+            CurrentRed = 0;
+        }
+
+        private bool CanAdd(object parameter)
+        {  
+            return true;
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
